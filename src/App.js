56835f805f278594ce27db19766e8450924b59d5
Derrick Strong <derrickmstrong@gmail.com>
2022-04-04 14:02:38 -0500

    Added GitHub api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
+import { useCallback, useEffect, useState } from 'react';
+import { githubDB } from './db';
+import { githubQuery } from './query'
+
 function App() {
+  const [state, setState] = useState({
+    name: '',
+    repoList: [],
+  })
+  const fetchData = useCallback(() => { 
+    fetch(githubDB.baseURL, {
+      method: 'POST',
+      headers: githubDB.headers,
+      body: JSON.stringify(githubQuery),
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        const { name, repositories } = data.data.viewer;
+        setState({
+          name: name,
+          repoList: repositories.nodes
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      })
+   }, [])
+
+  useEffect(() => {
+    fetchData()
+  },[fetchData]);
+
   return (
-    <div className="container mt-5">
-      <h1 className="text-primary"><i className="bi bi-diagram-2-fill"></i>Repos</h1>
+    <div className='container mt-5'>
+      <h1 className='text-primary'>
+        <i className='bi bi-diagram-2-fill'></i>Repos
+      </h1>
+      <p>{state.name}</p>
+      <ul>
+        {state.repoList.map(({id, name, url}) => {
+          return (
+            <li key={id}>{name} - {url}</li>
+          )
+        })}
+      </ul>
     </div>
   );
 }
---
+++ src/query.js
@@ -0,0 +1,17 @@
+export const githubQuery = {
+  query: `
+      {
+        viewer {
+          name
+          repositories(first: 10){
+            nodes{
+              name
+              description
+              id
+              url
+            }
+          }
+        }
+      }
+      `,
+};
